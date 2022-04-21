9a355583d8b476ed3ad2dec22fd0220709eba98e
KaioMachado <kaiobmachado@gmail.com>
2022-04-21 15:59:08 -0300

    criando calculadora

---
+++ calcula.js
@@ -0,0 +1,14 @@
+
+
+
+var numeroInicial = 0;
+var operacao =  "null";
+var numeroFinal = 0 ; 
+var resultado = 0;
+
+function iniciaOperacao (){
+    document.querySelector(".displayOperacao")
+
+}
+
+console.log("resultado");
